1e3ea9ca23c8138dddff86f40de3a2f163bdfe6b
snapdragon teaks <97859152+Elchanz3@users.noreply.github.com>
2023-03-08 14:21:25 -0300

    build env: add build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir out
+
+DTB_DIR=$(pwd)/out/arch/arm64/boot/dts
+mkdir ${DTB_DIR}/exynos
+
+export PLATFORM_VERSION=11
+export ANDROID_MAJOR_VERSION=r
+export SEC_BUILD_CONF_VENDOR_BUILD_OS=12
+
+make O=out ARCH=arm64 exynos9820-d2s_defconfig
+
+make O=out ARCH=arm64 -j8
+
+$(pwd)/tools/mkdtimg cfg_create $(pwd)/out/dtb.img dt.configs/exynos9820.cfg -d ${DTB_DIR}/exynos
+
+IMAGE="out/arch/arm64/boot/Image"
+if [[ -f "$IMAGE" ]]; then
+	rm AnyKernel3/zImage > /dev/null 2>&1
+	rm AnyKernel3/dtb > /dev/null 2>&1
+	rm AnyKernel3/*.zip > /dev/null 2>&1
+	mv out/dtb.img AnyKernel3/dtb
+	mv $IMAGE AnyKernel3/zImage
+	cd AnyKernel3
+	zip -r9 Kernel-N975F.zip .
+fi
